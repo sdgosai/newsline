c665447d3421bf2ea49baa6bf0f5303e61fe0fc2
Sachingiri Gosai <sachingosai.flexion@gmail.com>
2022-04-12 10:56:28 +0530

    - manage space

--- src/controller/NewsController.js
+++ src/controller/NewsController.js
@@ -87,8 +87,6 @@ exports.categoryNews = async (req, res, next) => {
             news: news
         });
         return res.send(rec)
-        console.log(res);
-        console.log("hello");
     } catch (e) {
         console.log(e);
         return res.send({ success: false, message: e.message })
